e37e3362292dfeba5ffd6279579dfc5928cd944b
imzak31 <adriandenb@gmail.com>
2024-07-05 01:12:52 -0400

    [#13]: Require dotenv rails in prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'active_support/core_ext/integer/time'
+require 'dotenv-rails'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -32,7 +33,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :google
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
